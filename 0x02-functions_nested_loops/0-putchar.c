e0fd97e68b6e4293ee7ab5cc717b4e10b4100478
Okoli Nnaemeka <okolinnaemeka227@gmail.com>
2022-07-05 14:45:25 -0700

    putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,13 +1,19 @@
-#include <stdio.h>
-
+#include "0-putchar.h"
 /**
  * main - Entry point
- * Return:0
+ *
+ * Return: Always 0 (Success)
  */
-
 int main(void)
 {
-printf("_putchar");
+char *sh = "_putchar";
+
+while (*sh)
+{
+_putchar(*sh);
+sh++;
+}
 _putchar('\n');
+
 return (0);
 }
