75cdabe4d3b652aea6b64593f33a281586bd7eda
Okoli Nnaemeka <okolinnaemeka227@gmail.com>
2022-07-06 16:40:39 -0700

    first

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,11 +1,9 @@
 #include "main.h"
 
 /**
- * main - check the code
+ * print_alphabet - To print alphabet lowercase
  *
- * Description: 'print_alphabet - print alphabet lowercase'
- *
- * Return: Always 0.
+ * Return: Always 0 (Success)
  */
 
 void print_alphabet(void)
--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,18 +1,19 @@
 #include "main.h"
-
+/**
+ * print_alphabet_x10 - To print 10 times
+ */
 
 void print_alphabet_x10(void)
 {
-   
-int j =0;
-for (j=0 ; j<10 ; j++)
+int j = 0;
+for (j = 0 ; j < 10 ; j++)
 {
-char i ='a';
-while (i <= 'z') {
+char i = 'a';
+while (i <= 'z')
+{
 _putchar(i);
 i++;
 }
 _putchar('\n');
 }
-
 }
